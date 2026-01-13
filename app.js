af96cafb1231d5251c09776c8b790fdf7156d990
rahulshendre <rahulshendre789@gmail.com>
2026-01-13 12:58:04 +0530

    final checks for the workflow
    Signed-off-by: rahulshendre <rahulshendre789@gmail.com>

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello, Kubernetes!'));
+app.get('/', (req, res) => res.send('CI Pipeline is working!'));
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
